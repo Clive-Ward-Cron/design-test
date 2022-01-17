c52c31d28a308fa057fe6f430b60d87ade12d10d
Clive-Ward-Cron <clive_wardcron@live.com>
2022-01-17 11:04:45 -0700

    Got the slider function working nicer. Not ideal.

--- assets/index.js
+++ assets/index.js
@@ -2,7 +2,14 @@ const content = document.querySelector(".content");
 const slider = content.querySelector(".slider");
 
 function slideContent(e) {
-  content.classList.toggle("slide");
+  const [slideTime] = content.dataset.slide.split("s");
+  console.log(slideTime);
+  content.style = `--slide-time: ${slideTime}s`;
+  content.classList.toggle("slide-open");
+  // setTimeout(() => {
+  //   content.classList.toggle("slide");
+  // content.classList.toggle("content-open");
+  // }, parseFloat(slideTime) * 1000);
 }
 
 slider.addEventListener("click", slideContent);
